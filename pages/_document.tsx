8a49e2119c9ee9aa935a16ff765cd8fd49e26e4d
insoo <insoo2846@gmail.com>
2022-03-15 14:34:49 +0900

    Fix: _document link 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -50,6 +50,15 @@ class CustomDocument extends Document {
                     <meta name="description" content="" />
                     <meta name="keywords" content="" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link
+                        rel="stylesheet"
+                        type="text/css"
+                        href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                        rel="stylesheet"
+                    />
                     {/*<link*/}
                     {/*    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800;900&display=swap"*/}
                     {/*    rel="preload"*/}
