f869b89005e3bf666b9b6f25114bd75c40c309b8
insoo <insoo2846@gmail.com>
2022-03-16 14:26:40 +0900

    Add : 스크롤 감시 훅 작성

---
+++ hooks/useScroll.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import { throttle } from 'lodash'
+
+export const useScroll = (throttleDuration: number = 200) => {
+    const [float, setFloat] = useState(false)
+
+    useEffect(() => {
+        if (window.scrollY !== 0) {
+            setFloat(true)
+        }
+        const listener = throttle(() => {
+            if (window.scrollY !== 0) {
+                !float && setFloat(true)
+            } else {
+                float && setFloat(false)
+            }
+        }, throttleDuration)
+        window.addEventListener('scroll', listener)
+
+        return () => {
+            window.removeEventListener('scroll', listener)
+        }
+    }, [float])
+
+    return float
+}
