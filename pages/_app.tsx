7ad2a408cdd245a5141af98c9158f9cad5db077a
insoo <insoo2846@gmail.com>
2022-03-16 14:29:24 +0900

    Fix: _app.tsx 글로벌 스타일 및 Title 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from '../styles/GlobalStyles'
+import { GlobalStyles as TwGlobal } from 'twin.macro'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <title>insoodev</title>
+            </Head>
+            <TwGlobal />
+            <GlobalStyles />
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
